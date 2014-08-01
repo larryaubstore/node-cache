4dd13d49051732bda5cf14b228ae91babf8a0185
Laurence Morin-Daoust <laurence.morin.daoust@gmail.com>
2014-08-01 08:08:44 -0400

    Minimun key pointer added for top list.

--- test/spec/topRequestSpec.js
+++ test/spec/topRequestSpec.js
@@ -56,8 +56,67 @@ describe("Put key/value/ranking tests", function () {
     expect(cache.toprequestkey["key4"]).toEqual(1);
     expect(cache.toprequestkey["key5"]).toEqual(1);
 
-    var test = Object.keys(cache.toprequestkeycandidates);
     expect(cache.toprequestkeycandidates["key6"]).toBeUndefined();
+  });
+
+  it("Test toprequestkey increment", function () {
+
+    var cache  = new nodecache.Nodecache();
+
+    spyOn(cache, "cleanarray").andCallFake(function () {});
+    cache.addtoprequestkey("key1");
+    cache.addtoprequestkey("key2");
+    cache.addtoprequestkey("key3");
+    cache.addtoprequestkey("key4");
+    cache.addtoprequestkey("key5");
+    cache.addtoprequestkey("key6");
+    cache.addtoprequestkey("key7");
+    cache.addtoprequestkey("key8");
+    cache.addtoprequestkey("key9");
+    cache.addtoprequestkey("key10");
+    cache.addtoprequestkey("key11");
+    cache.addtoprequestkey("key1");
+
+    expect(cache.toprequestkey["key1"]).toEqual(2);
+    expect(cache.toprequestkey["key2"]).toEqual(1);
+    expect(cache.toprequestkey["key3"]).toEqual(1);
+    expect(cache.toprequestkey["key4"]).toEqual(1);
+    expect(cache.toprequestkey["key5"]).toEqual(1);
+
+  });
+
+  it("Test toprequestkey increment", function () {
+
+    var cache  = new nodecache.Nodecache();
+
+    spyOn(cache, "cleanarray").andCallFake(function () {});
+    cache.addtoprequestkey("key1");
+    cache.addtoprequestkey("key1");
+    cache.addtoprequestkey("key2");
+    cache.addtoprequestkey("key2");
+    cache.addtoprequestkey("key3");
+    cache.addtoprequestkey("key3");
+    cache.addtoprequestkey("key4");
+    cache.addtoprequestkey("key4");
+    cache.addtoprequestkey("key5");
+    cache.addtoprequestkey("key5");
+
+
+    expect(cache.toprequestkey["key1"]).toEqual(2);
+    expect(cache.toprequestkey["key2"]).toEqual(2);
+    expect(cache.toprequestkey["key3"]).toEqual(2);
+    expect(cache.toprequestkey["key4"]).toEqual(2);
+    expect(cache.toprequestkey["key5"]).toEqual(2);
+
+    cache.addtoprequestkey("key6");
+    expect(cache.toprequestkeycandidates["key6"]).toEqual(1);
+    cache.addtoprequestkey("key6");
+    expect(cache.toprequestkeycandidates["key6"]).toEqual(2);
+
+    // SWAP
+    cache.addtoprequestkey("key6");
+    expect(cache.toprequestkey["key6"]).toEqual(3);
+    expect(cache.toprequestkey["key1"]).toBeUndefined();
   });
 
 });
