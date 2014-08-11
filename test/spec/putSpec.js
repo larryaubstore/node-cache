b1fa38d5879975cfa2ee9fc4a0dd65ab441a3d3f
Laurence Morin-Daoust <laurence.morin.daoust@gmail.com>
2014-08-10 23:04:09 -0400

    A few bugs fixed.

--- test/spec/putSpec.js
+++ test/spec/putSpec.js
@@ -47,8 +47,8 @@ describe("Put key/value/ranking tests", function () {
   it("cleanarray method test", function () {
 
     var cache = new nodecache.Nodecache();
-    cache.status.average = 2001;
-    cache.status.count = 4;
+    cache.status.average = 2501;
+    cache.status.count = 400;
 
     cache.array["first"]   = {ranking: 1000, value: "firstvalue"};
     cache.array["second"]  = {ranking: 2000, value: "secondvalue"};
@@ -80,5 +80,61 @@ describe("Put key/value/ranking tests", function () {
     expect(cache.cleanarray).toHaveBeenCalled();
     expect(cache.cleanarray.mostRecentCall.args[0]).toEqual(1);
   });
+
+  it("memorysize property decreased", function () {
+    var cache = new nodecache.Nodecache({maxsize: 30});
+    cache.put(1000, "key2", "123456789012345");
+    expect(cache.status.memorysize).toEqual(20);
+    expect(cache.status.average).toEqual(1000);
+
+    cache.put(2000, "key3", "1234567890123456");
+    expect(cache.status.memorysize).toEqual(21);
+    expect(cache.status.average).toEqual(2000);
+  });
+
+  it("test average - simple case", function () {
+    var cache = new nodecache.Nodecache({maxsize: 1000});
+    cache.put(1000, "key2", "123456789012345");
+    expect(cache.status.memorysize).toEqual(20);
+    expect(cache.status.average).toEqual(1000);
+
+    cache.put(1000, "key3", "1234567890123456");
+    expect(cache.status.memorysize).toEqual(41);
+    expect(cache.status.average).toEqual(1000);
+  });
+
+  it("test average - simple case #2", function () {
+    var cache = new nodecache.Nodecache({maxsize: 1000});
+    cache.put(1000, "key2", "123456789012345");
+    expect(cache.status.memorysize).toEqual(20);
+    expect(cache.status.average).toEqual(1000);
+
+    cache.put(2000, "key3", "1234567890123456");
+    expect(cache.status.memorysize).toEqual(41);
+    expect(cache.status.average).toEqual(1500);
+  });
+
+  it("test average - simple case #3", function () {
+    var cache = new nodecache.Nodecache({maxsize: 1000});
+    cache.put(1000, "key2", "123456789012345");
+    expect(cache.status.memorysize).toEqual(20);
+    expect(cache.status.average).toEqual(1000);
+
+    cache.put(2001, "key3", "1234567890123456");
+    expect(cache.status.memorysize).toEqual(41);
+    expect(cache.status.average).toEqual(1500);
+  });
+
+  it("test average - simple case #4", function () {
+    var cache = new nodecache.Nodecache({maxsize: 1000});
+    cache.put(1000, "key2", "123456789012345");
+    expect(cache.status.memorysize).toEqual(20);
+    expect(cache.status.average).toEqual(1000);
+
+    cache.put(2001, "key2", "123456789012345");
+    expect(cache.status.memorysize).toEqual(20);
+    expect(cache.status.average).toEqual(2001);
+  });
+
 
 });
