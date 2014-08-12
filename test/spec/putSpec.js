8a3d662b43255dff19e845902fdfc2bcbdf756a7
Laurence Morin-Daoust <laurence.morin.daoust@gmail.com>
2014-08-12 09:37:55 -0400

    Partial commit.

--- test/spec/putSpec.js
+++ test/spec/putSpec.js
@@ -92,49 +92,6 @@ describe("Put key/value/ranking tests", function () {
     expect(cache.status.average).toEqual(2000);
   });
 
-  it("test average - simple case", function () {
-    var cache = new nodecache.Nodecache({maxsize: 1000});
-    cache.put(1000, "key2", "123456789012345");
-    expect(cache.status.memorysize).toEqual(20);
-    expect(cache.status.average).toEqual(1000);
-
-    cache.put(1000, "key3", "1234567890123456");
-    expect(cache.status.memorysize).toEqual(41);
-    expect(cache.status.average).toEqual(1000);
-  });
-
-  it("test average - simple case #2", function () {
-    var cache = new nodecache.Nodecache({maxsize: 1000});
-    cache.put(1000, "key2", "123456789012345");
-    expect(cache.status.memorysize).toEqual(20);
-    expect(cache.status.average).toEqual(1000);
-
-    cache.put(2000, "key3", "1234567890123456");
-    expect(cache.status.memorysize).toEqual(41);
-    expect(cache.status.average).toEqual(1500);
-  });
-
-  it("test average - simple case #3", function () {
-    var cache = new nodecache.Nodecache({maxsize: 1000});
-    cache.put(1000, "key2", "123456789012345");
-    expect(cache.status.memorysize).toEqual(20);
-    expect(cache.status.average).toEqual(1000);
-
-    cache.put(2001, "key3", "1234567890123456");
-    expect(cache.status.memorysize).toEqual(41);
-    expect(cache.status.average).toEqual(1500);
-  });
-
-  it("test average - simple case #4", function () {
-    var cache = new nodecache.Nodecache({maxsize: 1000});
-    cache.put(1000, "key2", "123456789012345");
-    expect(cache.status.memorysize).toEqual(20);
-    expect(cache.status.average).toEqual(1000);
-
-    cache.put(2001, "key2", "123456789012345");
-    expect(cache.status.memorysize).toEqual(20);
-    expect(cache.status.average).toEqual(2001);
-  });
 
 
 });
