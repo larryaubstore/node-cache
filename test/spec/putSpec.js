9b834ec447e7ccaa8278650ace1555d3bc511400
Laurence Morin-Daoust <laurence.morin.daoust@gmail.com>
2014-09-04 22:41:46 -0400

    Partial commit.

--- test/spec/putSpec.js
+++ test/spec/putSpec.js
@@ -53,9 +53,9 @@ describe("Put key/value/ranking tests", function () {
 
   it("test cache limit", function () {
 
-    var cache  = new nodecache.Nodecache({limit: 5});
+    var cache  = new nodecache.Nodecache({limit: 6});
 
-    expect(cache.redblack.limit).toEqual(5);
+    expect(cache.redblack.limit).toEqual(6);
 
     spyOn(cache.redblack, "insert").andCallThrough();
     spyOn(cache.redblack, "delete").andCallThrough();
@@ -63,29 +63,38 @@ describe("Put key/value/ranking tests", function () {
    
     cache.put("key", "keyvalue");
     cache.put("key", "keyvalue");
-    expect(cache.redblack.count).toEqual(1);
+    expect(cache.redblack.count).toEqual(2);
+    expect(cache.redblack.root.key).toEqual(1);
+    expect(cache.redblack.root.value).toEqual(["key", "key"]);
+  
+
+    expect(Object.keys(cache.array).length).toEqual(1);
+    expect(cache.get("key").value).toEqual(["keyvalue", "keyvalue"]);
 
     cache.put("key1", "one");
-    expect(cache.redblack.count).toEqual(2);
-
-    cache.put("key2", "two");
     expect(cache.redblack.count).toEqual(3);
 
-    cache.put("key3", "three");
+    cache.put("key2", "two");
     expect(cache.redblack.count).toEqual(4);
+
+    cache.put("key3", "three");
+    expect(cache.redblack.count).toEqual(5);
 
 
     var element = cache.get("key");
     expect(element).not.toBeUndefined();
     expect(element.ranking).toEqual(2);
     expect(element.value).toEqual(["keyvalue", "keyvalue"]);
-    expect(cache.redblack.count).toEqual(4);
+    expect(cache.redblack.count).toEqual(5);
 
     element = cache.get("key2");
     expect(element).not.toBeUndefined();
     expect(element.ranking).toEqual(1);
     expect(element.value).toEqual(["two"]);
 
+    
+
+    debugger;
     cache.put("key4", "four");
 
 
@@ -155,32 +164,146 @@ describe("Put key/value/ranking tests", function () {
 
   });
 
-  it("real case", function () {
 
-    var fileread = false;
+  it("test simple scenario", function () {
 
-    var array = [];
-    fs.readFile("test/spec/testarray.txt", 'utf8', function (err,data) {
+    var cache  = new nodecache.Nodecache({limit: 6});
 
-      debugger;
-      if(data) {
-        array = data.toString().split('\n'); // find position of new line element
-        fileread = true;
-      }
-
-    });
+    spyOn(cache.redblack, "insert").andCallThrough();
+    spyOn(cache.redblack, "delete").andCallThrough();
 
 
-    waitsFor(function () {
-      return fileread == true;
-    }, "File never read", 2000);
+    cache.put("test", " ");
+    // INSERT 1
+    expect(cache.redblack.insert.calls.length).toEqual(1);
+    expect(cache.redblack.insert.calls[0].args[0]).toEqual(1);
+    
 
-    runs(function () {
 
-      expect(array.length).not.toEqual(0);
-    });
+    cache.put("test", " ");
+    // INSERT 2
+    expect(cache.redblack.insert.calls.length).toEqual(2);
+    expect(cache.redblack.insert.calls[1].args[0]).toEqual(2);
+    // DELETE 1
+    expect(cache.redblack.delete.calls.length).toEqual(1);
+    expect(cache.redblack.delete.calls[0].args[0]).toEqual(1);
+
+    expect(cache.redblack.root.key).toEqual(2);
+    expect(cache.redblack.root.left).toEqual(null);
+    expect(cache.redblack.root.right).toEqual(null);
+
+    expect(Object.keys(cache.array).length).toEqual(1);
+
+
+    cache.put("test1", " ");
+    // INSERT 1
+    expect(cache.redblack.insert.calls.length).toEqual(3);
+    expect(cache.redblack.insert.calls[2].args[0]).toEqual(1);
+    expect(cache.redblack.delete.calls.length).toEqual(1);
+  });
+
+  it("real case #2", function () {
+
+
+    var cache  = new nodecache.Nodecache({limit: 6});
+    
+    cache.put("don", "k");
+    // don = 1
+    expect(cache.redblack.count).toEqual(1);
+
+    cache.put("dont", "k");
+    // dont = 1
+    expect(cache.redblack.count).toEqual(2);
+
+    cache.put("donner", "k");
+    // dois = 1
+    expect(cache.redblack.count).toEqual(3);
+
+    cache.put("dois", "k");
+    // dois = 1
+    expect(cache.redblack.count).toEqual(4);
+
+    cache.put("don", "k");
+    // don = 1 == 2
+    expect(cache.redblack.count).toEqual(1);
+
+    cache.put("dormais", "k");
+    // dormais = 1
+
+    cache.put("docteurs", "k");
+    // docteurs = 1 = 0
+
+
+    cache.put("dont", "k");
+
+
+    
+
+    //KEY ==> don || VALUE ==> keyvalue
+    //INSERT ==> 1
+    //KEY ==> dont || VALUE ==> keyvalue
+    //INSERT ==> 1
+    //KEY ==> donner || VALUE ==> keyvalue
+    //INSERT ==> 1
+    //KEY ==> dois || VALUE ==> keyvalue
+    //INSERT ==> 1
+    //KEY ==> don || VALUE ==> keyvalue
+    //DELETE ==> 1
+    //INSERT ==> 2
+    //KEY ==> dormais || VALUE ==> keyvalue
+    //INSERT ==> 1
+    //KEY ==> docteurs || VALUE ==> keyvalue
+    //INSERT ==> 1
+    //DELETE ==> 1
+    //KEY ==> dont || VALUE ==> keyvalue
+    //DELETE ==> 1
+    //INSERT ==> 2
+    //DELETE ==> 2
+    //7
+
 
   });
+
+//  it("real case", function () {
+//
+//    console.log("\n\n\n************************************");
+//    var cache  = new nodecache.Nodecache({limit: 6});
+//    var fileread = false;
+//
+//    var array = [];
+//    fs.readFile("test/spec/testarray.txt", 'utf8', function (err,data) {
+//      if(data) {
+//        array = data.toString().split('\n'); // find position of new line element
+//        fileread = true;
+//      }
+//    });
+//
+//
+//    waitsFor(function () {
+//      return fileread == true;
+//    }, "File never read", 2000);
+//
+//    runs(function () {
+//
+//      expect(array.length).not.toEqual(0);
+//
+//      for(var i = 0; i < array.length; i++) {
+//
+//        if(i <= 7) {
+//          debugger;
+//          cache.put(array[i], "keyvalue");
+//          if(cache.redblack.root == null) {
+//            console.log(i);
+//          }
+//        }
+//      }
+//
+//      var allkeys = Object.keys(cache.array);
+//      debugger;
+//      
+//    });
+//
+//  });
 
 
 
